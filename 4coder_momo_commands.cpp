f59b71ff81b7c2fc3376d61ca5570d28863bf50d
Gerald Wong <email@geraldwong.net>
2021-04-16 17:05:47 +0800

    added on/off #if/#endif command

--- 4coder_momo_commands.cpp
+++ 4coder_momo_commands.cpp
@@ -11,32 +11,32 @@ static b32 global_insert_mode = false;
 function void
 momo_write_text_and_auto_indent_internal(Application_Links* app, String_Const_u8 insert) {
     ProfileScope(app, "write and auto indent");
-    if (insert.str != 0 && insert.size > 0){
+    if (insert.str != 0 && insert.size > 0) {
         b32 do_auto_indent = false;
-        for (u64 i = 0; !do_auto_indent && i < insert.size; i += 1){
-            switch (insert.str[i]){
-                case ';': case ':':
-                case '{': case '}':
-                case '(': case ')':
-                case '[': case ']':
-                case '#':
-                case '\n': case '\t':
-                {
-                    do_auto_indent = true;
-                }break;
+        for (u64 i = 0; !do_auto_indent && i < insert.size; i += 1) {
+            switch (insert.str[i]) {
+            case ';': case ':':
+            case '{': case '}':
+            case '(': case ')':
+            case '[': case ']':
+            case '#':
+            case '\n': case '\t':
+            {
+                do_auto_indent = true;
+            }break;
             }
         }
-        if (do_auto_indent){
+        if (do_auto_indent) {
             View_ID view = get_active_view(app, Access_ReadWriteVisible);
             Buffer_ID buffer = view_get_buffer(app, view, Access_ReadWriteVisible);
             Range_i64 pos = {};
             pos.min = view_get_cursor_pos(app, view);
             write_text(app, insert);
-            pos.max= view_get_cursor_pos(app, view);
+            pos.max = view_get_cursor_pos(app, view);
             auto_indent_buffer(app, buffer, pos, 0);
             move_past_lead_whitespace(app, view, buffer);
         }
-        else{
+        else {
             write_text(app, insert);
         }
     }
@@ -47,14 +47,15 @@ momo_boundary_word(Application_Links* app, Buffer_ID buffer, Side side, Scan_Dir
     i64 result = 0;
     if (direction == Scan_Forward) {
         result = Min(buffer_seek_character_class_change_0_1(app, buffer, &character_predicate_alpha_numeric_underscore_utf8, direction, pos),
-                     buffer_seek_character_class_change_1_0(app, buffer, &character_predicate_alpha_numeric_underscore_utf8, direction, pos));
+            buffer_seek_character_class_change_1_0(app, buffer, &character_predicate_alpha_numeric_underscore_utf8, direction, pos));
         result = Min(result, buffer_seek_character_class_change_1_0(app, buffer, &character_predicate_whitespace, direction, pos));
-    } else {
+    }
+    else {
         result = Max(buffer_seek_character_class_change_0_1(app, buffer, &character_predicate_alpha_numeric_underscore_utf8, direction, pos),
-                     buffer_seek_character_class_change_1_0(app, buffer, &character_predicate_alpha_numeric_underscore_utf8, direction, pos));
+            buffer_seek_character_class_change_1_0(app, buffer, &character_predicate_alpha_numeric_underscore_utf8, direction, pos));
         result = Max(result, buffer_seek_character_class_change_1_0(app, buffer, &character_predicate_whitespace, direction, pos));
     }
-    
+
     return result;
 }
 
@@ -116,15 +117,15 @@ CUSTOM_DOC("Page down halfway")
 CUSTOM_COMMAND_SIG(momo_switch_to_edit_mode)
 CUSTOM_DOC("EDIT MODE ACTIVATED")
 {
-	switch_to_keybinding_0(app);
-	global_insert_mode = false;
+    switch_to_keybinding_0(app);
+    global_insert_mode = false;
 }
 
 CUSTOM_COMMAND_SIG(momo_switch_to_insert_mode)
 CUSTOM_DOC("INSERT MODE ACTIVATED")
 {
-	switch_to_keybinding_1(app);
-	global_insert_mode = true;
+    switch_to_keybinding_1(app);
+    global_insert_mode = true;
 }
 
 CUSTOM_COMMAND_SIG(w)
@@ -175,7 +176,7 @@ CUSTOM_DOC("Inserts text and auto-indents the line on which the cursor sits if a
 
 CUSTOM_COMMAND_SIG(momo_append_line_and_enter_insert)
 CUSTOM_DOC("Inserts text and auto-indents the line on which the cursor sits if any of the text contains 'layout punctuation' such as ;:{}()[]# and new lines.")
-{  
+{
     seek_end_of_textual_line(app);
     momo_write_text_and_auto_indent_internal(app, string_u8_litexpr("\n"));
     momo_switch_to_insert_mode(app);
@@ -193,11 +194,11 @@ CUSTOM_DOC("Cut the text in the range from the cursor to the mark onto the clipb
     i32 size = (i32)buffer_get_size(app, buffer);
     range.end = clamp_top(range.end, size);
     if (range_size(range) == 0 ||
-        buffer_get_char(app, buffer, range.end - 1) != '\n'){
+        buffer_get_char(app, buffer, range.end - 1) != '\n') {
         range.start -= 1;
         range.first = clamp_bot(0, range.first);
     }
-    if (clipboard_post_buffer_range(app, 0, buffer, range)){
+    if (clipboard_post_buffer_range(app, 0, buffer, range)) {
         buffer_replace_range(app, buffer, range, string_u8_empty);
     }
 }
@@ -237,7 +238,7 @@ CUSTOM_DOC("Vsplit panel but don't go")
     View_ID view = get_active_view(app, Access_Always);
     View_ID new_view = open_view(app, view, ViewSplit_Right);
     new_view_settings(app, new_view);
-    
+
 }
 
 
@@ -256,7 +257,7 @@ CUSTOM_DOC("Change mode")
 {
     View_ID view = get_active_view(app, Access_ReadWriteVisible);
     Buffer_ID buffer = view_get_buffer(app, view, Access_ReadWriteVisible);
-    if (buffer != 0){
+    if (buffer != 0) {
         Query_Bar_Group group(app);
         Query_Bar bar = {};
         bar.prompt = string_u8_litexpr("Change mode\n");
@@ -265,13 +266,13 @@ CUSTOM_DOC("Change mode")
         User_Input in = {};
         for (;;) {
             in = get_next_input(app, EventProperty_AnyKey, EventProperty_MouseButton);
-            if (in.abort || match_key_code(&in, KeyCode_Escape)){
+            if (in.abort || match_key_code(&in, KeyCode_Escape)) {
                 break;
             }
 
             else if (match_key_code(&in, KeyCode_I)) {
                 in = get_next_input(app, EventProperty_AnyKey, EventProperty_MouseButton);
-                if (in.abort || match_key_code(&in, KeyCode_Escape)){
+                if (in.abort || match_key_code(&in, KeyCode_Escape)) {
                     break;
                 }
 
@@ -297,7 +298,7 @@ CUSTOM_DOC("Change mode")
                     break;
                 }
             }
-                
+
         }
     }
 }
@@ -307,7 +308,8 @@ CUSTOM_DOC("Toggle recording")
 {
     if (global_keyboard_macro_is_recording) {
         keyboard_macro_finish_recording(app);
-    } else {
+    }
+    else {
         keyboard_macro_start_recording(app);
     }
 }
@@ -317,7 +319,7 @@ CUSTOM_DOC("Window manipulation mode")
 {
     View_ID view = get_active_view(app, Access_ReadVisible);
     Buffer_ID buffer = view_get_buffer(app, view, Access_ReadVisible);
-    if (buffer != 0){
+    if (buffer != 0) {
         Query_Bar_Group group(app);
         Query_Bar bar = {};
         bar.prompt = string_u8_litexpr("Window action mode!\n");
@@ -326,7 +328,7 @@ CUSTOM_DOC("Window manipulation mode")
         User_Input in = {};
         for (;;) {
             in = get_next_input(app, EventProperty_AnyKey, EventProperty_MouseButton);
-            if (in.abort || match_key_code(&in, KeyCode_Escape)){
+            if (in.abort || match_key_code(&in, KeyCode_Escape)) {
                 break;
             }
 
@@ -354,18 +356,18 @@ CUSTOM_DOC("Window manipulation mode")
                 f4_toggle_compilation_expand(app);
                 break;
             }
-                
-            
+
+
         }
     }
 }
 
-CUSTOM_COMMAND_SIG(momo_if0_on) 
+CUSTOM_COMMAND_SIG(momo_if0_on)
 CUSTOM_DOC("Removes #if 0/#endif")
 {
     // check the current line the cursor is in
     Scratch_Block scratch(app);
-    
+
     View_ID view = get_active_view(app, Access_ReadWriteVisible);
     Buffer_ID buffer = view_get_buffer(app, view, Access_ReadWriteVisible);
     i64 pos = view_get_cursor_pos(app, view);
@@ -375,72 +377,114 @@ CUSTOM_DOC("Removes #if 0/#endif")
     i32 size = (i32)buffer_get_size(app, buffer);
     line_range.end = clamp_top(line_range.end, size);
     if (range_size(line_range) == 0 ||
-        buffer_get_char(app, buffer, line_range.end - 1) != '\n'){
+        buffer_get_char(app, buffer, line_range.end - 1) != '\n') {
         line_range.start -= 1;
         line_range.first = clamp_bot(0, line_range.first);
     }
     String_Const_u8 selected_str = push_buffer_range(app, scratch, buffer, line_range);
+    selected_str = string_skip_chop_whitespace(selected_str);
 
+    String_Const_u8 selected_str3 = selected_str;
+    selected_str3.size = 3;
+
+    String_Const_u8 selected_str6 = selected_str;
+    selected_str6.size = 6;
 
     // TODO(Momo): It doesn't have to be explicitly #if 0, right?
-    String_Const_u8 if0_str = string_u8_litexpr("#if 0\n");
-    String_Const_u8 endif_str = string_u8_litexpr("#endif\n");
-    if (string_match(selected_str, if0_str)) {
-        i64 search_from = line_range.end;
+    String_Const_u8 if0_str = string_u8_litexpr("#if");
+    String_Const_u8 endif_str = string_u8_litexpr("#endif");
+    if (string_match(selected_str3, if0_str)) {
+        // Have to -1 here. Otherwise, we might accidentally skip 
+        // two #ifs that are side by side.
+        i64 search_from = line_range.end - 1;
+
+        u32 if0_count = 1;
+        u32 endif_count = 0; // remember to count current line
+
         for (;;) {
             // find the next #endif or #if0
             i64 endif_pos;
-            i64 if0_pos;    
+            i64 if0_pos;
+         
             momo_seek_string_forward(app, buffer, -1, search_from, 0, if0_str, &if0_pos);
             momo_seek_string_forward(app, buffer, -1, search_from, 0, endif_str, &endif_pos);
-            if (endif_pos == -1) {
+
+            if (if0_pos != -1) {
+                if (if0_pos < endif_pos) {
+                    search_from = if0_pos;
+                    ++if0_count;
+                }
+            }
+
+            if (endif_pos != -1) {
+                if (if0_pos > endif_pos || if0_pos == -1) {
+                    search_from = endif_pos;
+                    ++endif_count;
+                }
+
+                // The question now is whether this #if0 is ours?
+                if (if0_count == endif_count) {
+                    // replace the #if
+                    buffer_replace_range(app, buffer, line_range, string_u8_empty);
+
+                    // replace the #endif
+                    i64 endif_line = get_line_number_from_pos(app, buffer, endif_pos);
+                    Range_i64 endif_line_range = get_line_pos_range(app, buffer, endif_line);
+                    buffer_replace_range(app, buffer, endif_line_range, string_u8_empty);
+                    break;
+                }
+
+            }
+            else {
+                // Here, we ran out of #endif
                 break;
             }
-            // if we found a #if0 behind a #endif, it is not the #endif we are
-            // looking for.
-            if (if0_pos != -1 && if0_pos < endif_pos) {
-                // not the endif we are looking for
-                search_from = endif_pos;
-                continue;
-            }
-              
-            // replace the #if 0
-            buffer_replace_range(app, buffer, line_range, string_u8_empty);
 
-            // replace is #endif
-            i64 endif_line = get_line_number_from_pos(app, buffer, endif_pos);
-            Range_i64 endif_line_range = get_line_pos_range(app, buffer, endif_line);
-            buffer_replace_range(app, buffer, endif_line_range, string_u8_empty);
-            break;
         }
     }
-    else if (string_match(selected_str, endif_str)) {
-        i64 search_from = line_range.end;
+    else if (string_match(selected_str6, endif_str)) {
+        i64 search_from = line_range.start;
+
+        u32 if0_count = 0;
+        u32 endif_count = 1; // remember to count current line
         for (;;) {
             // find the next #endif or #if0
             i64 endif_pos;
             i64 if0_pos;
             momo_seek_string_backward(app, buffer, -1, search_from, 0, if0_str, &if0_pos);
             momo_seek_string_backward(app, buffer, -1, search_from, 0, endif_str, &endif_pos);
-            if (if0_pos == -1) {
+           
+            if (endif_pos != -1) {
+                if (if0_pos < endif_pos) {
+                    search_from = endif_pos;
+                    ++endif_count;
+                }
+            }
+
+            if (if0_pos != -1) {
+                if (if0_pos > endif_pos || endif_pos == -1) {
+                    search_from = if0_pos;
+                    ++if0_count;
+                }
+
+                // The question now is whether this #if0 is ours?
+                if (if0_count == endif_count) {
+                    // replace the #endif
+                    buffer_replace_range(app, buffer, line_range, string_u8_empty);
+
+                    // replace the #if 
+                    i64 if0_line = get_line_number_from_pos(app, buffer, if0_pos);
+                    Range_i64 if0_line_range = get_line_pos_range(app, buffer, if0_line);
+                    buffer_replace_range(app, buffer, if0_line_range, string_u8_empty);
+                    break;
+                }
+
+            } 
+            else {
+                // Here, we ran out of #if0
                 break;
             }
 
-            // if we found an #endif in front of a #if, it is not the #if we are looking for 
-            if (endif_pos != -1 && if0_pos < endif_pos) {
-                // not the endif we are looking for
-                search_from = if0_pos;
-                continue;
-            }
-
-            // replace the #endif
-            buffer_replace_range(app, buffer, line_range, string_u8_empty);
-
-            // replace the #if 
-            i64 if0_line = get_line_number_from_pos(app, buffer, if0_pos);
-            Range_i64 if0_line_range = get_line_pos_range(app, buffer, if0_line);
-            buffer_replace_range(app, buffer, if0_line_range, string_u8_empty);
-            break;
         }
     }
 }
@@ -451,7 +495,7 @@ CUSTOM_DOC("Goto mode")
 {
     View_ID view = get_active_view(app, Access_ReadVisible);
     Buffer_ID buffer = view_get_buffer(app, view, Access_ReadVisible);
-    if (buffer != 0){
+    if (buffer != 0) {
         Query_Bar_Group group(app);
         Query_Bar bar = {};
         bar.prompt = string_u8_litexpr("Goto mode!\n");
@@ -460,21 +504,21 @@ CUSTOM_DOC("Goto mode")
         User_Input in = {};
         for (;;) {
             in = get_next_input(app, EventProperty_AnyKey, EventProperty_MouseButton);
-            if (in.abort || match_key_code(&in, KeyCode_Escape)){
+            if (in.abort || match_key_code(&in, KeyCode_Escape)) {
                 break;
             }
 
             if (match_key_code(&in, KeyCode_D)) {
-                Input_Modifier_Set *mods = &in.event.key.modifiers;
-                if (has_modifier(mods, KeyCode_Shift)){
+                Input_Modifier_Set* mods = &in.event.key.modifiers;
+                if (has_modifier(mods, KeyCode_Shift)) {
                     f4_go_to_definition_same_panel(app);
                 }
                 else {
                     f4_go_to_definition(app);
-                } 
+                }
                 break;
             }
-            
+
         }
     }
 }
@@ -485,7 +529,7 @@ CUSTOM_DOC("Queries the user a string, and can do reverse and forward search wit
 {
     View_ID view = get_active_view(app, Access_ReadVisible);
     Buffer_ID buffer = view_get_buffer(app, view, Access_ReadVisible);
-    if (buffer != 0){
+    if (buffer != 0) {
         Query_Bar_Group group(app);
         Query_Bar find = {};
         u8 find_buffer[1024];
@@ -493,8 +537,8 @@ CUSTOM_DOC("Queries the user a string, and can do reverse and forward search wit
         find.string = SCu8(find_buffer, (u64)0);
         find.string_capacity = sizeof(find_buffer);
 
-        if (query_user_string(app, &find)){
-            if (find.string.size > 0){
+        if (query_user_string(app, &find)) {
+            if (find.string.size > 0) {
 
                 Query_Bar bar = {};
                 bar.prompt = string_u8_litexpr("Navigate to: next(n), prev(N). Exit (Esc).\n");
@@ -510,15 +554,15 @@ CUSTOM_DOC("Queries the user a string, and can do reverse and forward search wit
                     isearch__update_highlight(app, view, match);
 
                     in = get_next_input(app, EventProperty_AnyKey, EventProperty_MouseButton);
-                    if (in.abort || match_key_code(&in, KeyCode_Escape)){
+                    if (in.abort || match_key_code(&in, KeyCode_Escape)) {
                         break;
                     }
 
                     if (match_key_code(&in, KeyCode_N)) {
-                        Input_Modifier_Set *mods = &in.event.key.modifiers;
-                        if (has_modifier(mods, KeyCode_Shift)){
+                        Input_Modifier_Set* mods = &in.event.key.modifiers;
+                        if (has_modifier(mods, KeyCode_Shift)) {
                             momo_seek_string_backward(app, buffer, match.start, match.start - 1, 0, find.string, &new_pos);
-                        } 
+                        }
                         else {
                             momo_seek_string_forward(app, buffer, match.start, match.end, 0, find.string, &new_pos);
                         }
@@ -527,7 +571,7 @@ CUSTOM_DOC("Queries the user a string, and can do reverse and forward search wit
                 }
 
                 view_disable_highlight_range(app, view);
-                if (in.abort){
+                if (in.abort) {
                     return;
                 }
                 view_set_cursor_and_preferred_x(app, view, seek_pos(new_pos));
